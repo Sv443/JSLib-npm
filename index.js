5d98350ee0017fc3e83d7c1c75365cf37c083143
Sven Fehler <sven.fehler@web.de>
2019-01-09 00:31:01 +0100

    1.7.0-prerelease

--- index.js
+++ index.js
@@ -332,3 +332,30 @@ module.exports.consoleColor = (text, colors) => {
     for(let i = 0; i < cnbr.length; i++) cnbr[i] = "\x1b[" + cnbr[i] + "m";
     return console.log(cnbr.join("") + text + "\x1b[0m");
 }
+
+/**
+ * Converts an array to a better readable one
+ * @param {Array} array The array you want to make readable
+ * @param {String} [separators=", "] The default separator for all values except the last one. Defaults to ", " if left empty. Add whitespaces if needed!
+ * @param {String} [lastSeparator=" and "] The last separator. Defaults to " and " if empty. Add whitespaces if needed!
+ * @returns {String} Better readable array as string
+ * @since 1.7.0
+ */
+module.exports.readableArray = (array, separators, lastSeparator) => {
+    if(isEmpty(array) || typeof array != "object" || (!isEmpty(separators) && typeof separators != "string" && typeof separators != "boolean") || (!isEmpty(lastSeparator) && typeof lastSeparator != "string" && typeof lastSeparator != "boolean")) return "wrong or missing arguments";
+    if(isEmptyWithoutString(lastSeparator) || lastSeparator === false) lastSeparator = " and ";
+    if(isEmptyWithoutString(separators) || separators === false) separators = ", ";
+
+    if(array.length <= 1) return array.toString();
+    else if(array.length == 2) return array.join(separators);
+    else {
+        let ae = lastSeparator + array[array.length - 1];
+        array.pop();
+        return array.join(separators) + ae;
+    }
+}
+
+function isEmptyWithoutString(variable) {
+    if((variable == null || variable == undefined || variable == [] || isNaN(variable)) && variable != "") return true;
+    else return false;
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+const jsl = require("./index.js");
+
+var arr = [1, 2, 3, 4, 5, "Apples", "Bananas"];
+
+console.log(jsl.readableArray(arr, "", ""));
