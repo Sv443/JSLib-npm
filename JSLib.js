dc4e6825f85858d344d2817ae932bfebe61f6f95
Sven <s.fehler@pbsnetwork.eu>
2019-09-19 12:42:01 +0200

    updated gitignore

--- JSLib.js
+++ JSLib.js
@@ -1,3 +1,4 @@
-// JSLib v1.8.0-npm by Sv443 - licensed under the MIT license
+// JSLib-npm by Sv443 - licensed under the MIT license
+// For more information, please read the `README.md` file or go to https://github.com/Sv443/JSLib-npm
 
 module.exports = require("./src/misc");
