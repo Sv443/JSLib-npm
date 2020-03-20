78a4f8bf68c9da115067f9cb3dcfc8fd2c6d6f53
Sv443 <sven.fehler@web.de>
2020-03-20 01:24:30 +0100

    version 1.9.1

--- src/functions/pause.js
+++ src/functions/pause.js
@@ -1,8 +1,9 @@
 /**
  * 🔹 Waits for the user to press a key and then resolves a Promise 🔹
  * @param {String} text The text to display
- * @returns {Promise} Passes the pressed key in the resolution or the error message in the rejection
+ * @returns {Promise<String>} Passes the pressed key in the resolution or the error message in the rejection
  * @since 1.9.0
+ * @version 1.9.1 Events are now being correctly unregistered
  */
 function pause(text = "Press any key to continue...")
 {
@@ -13,12 +14,19 @@ function pause(text = "Press any key to continue...")
         process.stdout.write(`${text} `);
         process.stdin.resume();
 
-        process.stdin.on("data", chunk => {
+        let onData = function(chunk)
+        {
+            if(/\u0003/gu.test(chunk)) // eslint-disable-line no-control-regex
+                process.exit(0);
+
             process.stdout.write("\n");
             process.stdin.pause();
 
+            process.stdin.removeListener("data", onData);
             return resolve(chunk.toString());
-        });
+        }
+
+        process.stdin.on("data", onData);
 
         process.stdin.on("error", err => {
             return reject(err);
